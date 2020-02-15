445851cc52c02c4166bcb3175409308873571158
Morten Astrup <morten.astrup@post.au.dk>
2020-02-15 13:37:44 +0100

    almost working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  constructor(props) {
+    super(props)
+    this.state = {apiResponse: ""};
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
 }
 
-export default App;
+componentWillMount() {
+  this.callAPI();
+}
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p className="App-intro">;{this.state.apiResponse}>
+         
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
