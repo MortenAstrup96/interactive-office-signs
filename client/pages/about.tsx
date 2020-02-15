324e6124ad900da666db86e39de3530fd06e228c
unknown <astrup.mort@gmail.com>
2020-02-15 22:30:41 +0100

    Switched to Next.js

---
+++ client/pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+export default function About() {
+    return(
+        <div>
+            <Layout>
+                <p>About page</p>
+            </Layout>
+        </div>
+    )
+}
---
+++ client/pages/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Header from '../components/header'
+import Layout from '../components/layout'
+
+export default function Index() {
+    return (
+        <div>
+            <Layout>
+                <p>Should work now!</p>
+            </Layout>
+            
+        </div>
+    )
+}
