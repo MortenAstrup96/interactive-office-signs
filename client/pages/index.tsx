203b6fcd16c874b7d9c5943ede2e79a6359cd48c
unknown <astrup.mort@gmail.com>
2020-02-17 09:27:56 +0100

    Next dynamic pages

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,11 +2,24 @@ import Link from 'next/link'
 import Header from '../components/header'
 import Layout from '../components/layout'
 
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
+
 export default function Index() {
     return (
         <div>
             <Layout>
-                <p>Should work now!</p>
+                <h1>Test</h1>
+                <ul>
+                    <PostLink title="Hello Next.js"/> 
+                    <PostLink title="This is a test"/>
+                    <PostLink title="Testing test"/>
+                </ul>
             </Layout>
             
         </div>
