f69a67aab4c456aee1afb3b4b697667cd57dfaec
Morten <morten@astrup.dev>
2020-02-24 10:27:45 +0100

    Hello World

--- webserver/pages/api/apiExample.ts
+++ webserver/pages/api/apiExample.ts
@@ -1,6 +1,16 @@
 import quotes from '../../quotes.json';
+
 export default (req, res) => {
-  const quote = quotes[Math.floor(Math.random() * quotes.length)];
-  res.status(200).json(quote);
+    switch (req.method) {
+      case 'GET': 
+        const quote = quotes[Math.floor(Math.random() * quotes.length)];
+        res.status(200).json(quote);
+        break
+      case 'POST':
+        //...
+        break
+      default:
+        res.status(405).end() //Method Not Allowed
+        break
+    }
   };
-  
