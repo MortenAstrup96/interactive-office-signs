e9f036a0c298da1ed516cfeceb29f42c4248be48
Morten <morten@astrup.dev>
2020-04-27 09:56:23 +0200

    :)

--- webserver/pages/api/uploadImageById/[nameId].ts
+++ webserver/pages/api/uploadImageById/[nameId].ts
@@ -1,8 +1,6 @@
 import {createWriteStream} from "fs";
 import path from "path";
 
-;
-
 export default async (req: any, res: any) => {
     // for the pipe to work, we need to disable "bodyParser" (see below)
     const {
