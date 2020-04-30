8c0b6e1084e496ba381d2f538de0ba795c582715
celinemoeller <celine@moellers.dk>
2020-04-30 09:18:55 +0200

    Added tabs with tabpanel

--- webserver/pages/user/[personId].tsx
+++ webserver/pages/user/[personId].tsx
@@ -4,7 +4,14 @@ import useSWR from "swr";
 import {UserInformation} from "../../library/general_interfaces";
 import {Customize} from "../../components/userConsole/customize";
 import {Status} from "../../components/userConsole/status";
-import {Tab, Tabs} from "@material-ui/core";
+import {Box, Tab, Tabs, Typography} from "@material-ui/core";
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: any;
+    value: any;
+}
 
 export default function Index() {
     const router = useRouter();
@@ -12,11 +19,37 @@ export default function Index() {
     let {data} = useSWR(() => '/api/user/' + router.query.personId, fetcher);
 
     /** Tab component */
-    const [value, setValue] = React.useState(0);
+    function TabPanel(props: TabPanelProps) {
+        const { children, value, index, ...other } = props;
 
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-        setValue(newValue);
-    };
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box p={2}>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
+    function allyProps(index: any) {
+        return {
+            id: `simple-tab${index}`,
+            'aria-controls': `simple-tabpanel-${index}`,
+        };
+    }
+
+        const [value, setValue] = React.useState(0);
+        const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+            setValue(newValue);
+        };
 
     useEffect(() => {
         if (data) {
@@ -60,27 +93,24 @@ export default function Index() {
         }
     }
 
-    console.log(currentUser?.status);
     if (!currentUser) return (<div> Loading... </div>);
     return (
         <div>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-            >
-                <Tab label="Profile" />
-                <Tab label="Status" />
-            </Tabs>
-        <Customize currentUser={currentUser} setCurrentUser={setCurrentUser} save={saveChanges}/>
-            <Status statusButtons={currentUser?.statusButtons} currentSelection={currentUser?.status}
-                    saveChanges={updateStatusButtons}/>
-            <Customize currentUser={currentUser} setCurrentUser={setCurrentUser} save={saveChanges}/>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    centered
+                    aria-label="simple tabs example"
+                >
+                    <Tab label="Profile" {...allyProps(0)} />
+                    <Tab label="Status" {...allyProps(1)} />
+                </Tabs>
+                <TabPanel value={value} index={0}>
+                    <Customize currentUser={currentUser} setCurrentUser={setCurrentUser} save={saveChanges}/>
+                </TabPanel>
+                <TabPanel index={1} value={value}>
+                    <Status statusButtons={currentUser?.statusButtons} currentSelection={currentUser?.status} saveChanges={updateStatusButtons}/>
+                </TabPanel>
         </div>
     );
 }
-
-// Customize component:
-//
